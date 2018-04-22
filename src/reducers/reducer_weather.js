035c500063cad36b0bc0318808389f9fc130027e
hasan330 <hasan.saleh.khader@gmail.com>
2018-04-22 12:43:27 +0300

    Never mutate state

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,14 @@
-export default function(state=null, action){
-	console.log("Action received: ", action);
+import { FETCH_WEATHER } from '../actions/index'
+
+export default function(state= [], action){
+
+	switch(action.type){
+		case FETCH_WEATHER:
+			console.log("Action received: ", action.payload.data);
+			
+			//Never mutate state	
+			return [action.payload.data, ...state];  // return  state.concat([action.payload.data]); (Equivalents)
+	}
+
 	return state;
 }
